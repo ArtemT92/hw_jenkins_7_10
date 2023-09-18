cf3c1fd0db82e5c589112e7f6abae564f72f3389
Artem Trunilin <a.trunilin92@mail.ru>
2023-09-18 22:19:07 +0300

    edits allure

--- hw_jenkins_7_10/pages/registration_page.py
+++ hw_jenkins_7_10/pages/registration_page.py
@@ -5,6 +5,7 @@ from hw_jenkins_7_10.pages.user import User
 import allure
 from allure_commons.types import Severity
 
+
 @allure.tag("web")
 @allure.severity(Severity.CRITICAL)
 @allure.label("owner", "artemtrunilin")
@@ -12,50 +13,64 @@ from allure_commons.types import Severity
 @allure.link("https://github.com", name="Testing")
 
 class RegistartionPage:
+    @allure.step('Открываем форму регистрации.')
     def open(self):
         browser.open('/')
 
+    @allure.step('Вводим имя пользователя {value}.')
     def fill_first_name(self, value):
         browser.element('#firstName').type(value)
 
+    @allure.step('Вводим фамилию пользователя {value}.')
     def fill_last_name(self, value):
         browser.element('#lastName').type(value)
 
+    @allure.step('Вводим email {value}.')
     def fill_email(self, value):
         browser.element('#userEmail').type(value)
 
+    @allure.step('Выбираем пол {value}.')
     def fill_gendare(self):
         browser.element('[for="gender-radio-1"').click()
 
+    @allure.step('Вводим телефонный номер {value}.')
     def fill_number(self, value):
         browser.element('#userNumber').type(value)
 
+    @allure.step('Вводим дату рождения {year}.{month}.{day}.')
     def fill_date_of_birth(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__month-select').send_keys(month)
         browser.element('.react-datepicker__year-select').send_keys(year)
         browser.element(f'.react-datepicker__day--0{day}').click()
 
+    @allure.step('Выводим предмет {value}.')
     def fill_subject(self, value):
         browser.element('#subjectsInput').type(value).press_enter()
 
+    @allure.step('Выбор хобби {value}.')
     def fill_hobbies(self):
         browser.element('[for="hobbies-checkbox-1"]').click()
 
+    @allure.step('Прикрепляем файл {name}.')
     def choose_picture(self, file_name):
         browser.element('[id="uploadPicture"]').send_keys(os.path.join(RESOURCE_PATH, file_name))
 
+    @allure.step('Вводим адрес {value}.')
     def fill_adress(self, value):
         browser.element('#currentAddress').type(value)
 
+    @allure.step('Ввод страны {value}.')
     def fill_state(self, value):
         browser.element('#state').click()
         browser.all('[id^=react-select][id*=option]').element_by(have.exact_text(value)).click()
 
+    @allure.step('Ввод города {value}.')
     def fill_city(self, value):
         browser.element('#city').click()
         browser.all('[id^=react-select][id*=option]').element_by(have.exact_text(value)).click()
 
+    @allure.step('Нажимаем кнопку Submit')
     def submit(self):
         browser.element('#submit').click()
 
@@ -74,6 +89,7 @@ class RegistartionPage:
         self.fill_city(user.city)
         self.submit()
 
+    @allure.step('Проверяем корректность введенных данных')
     def should_have_registered(self, user: User):
         browser.element('.table').all('td').even.should(
             have.exact_texts(f'{user.first_name} {user.last_name}',
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -4,15 +4,11 @@ from hw_jenkins_7_10.pages import user
 
 def test_registration_form():
     registartion_page = RegistartionPage()
-
-    @allure.step('Открываем главную страницу')
     registartion_page.open()
     student = user.student
 
     # When
-    @allure.step'Заполняем форму'
     registartion_page.fill(student)
 
     # Then
-    @allure.step'Проверяем регистрацию формы'
     registartion_page.should_have_registered(student)
