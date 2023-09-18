9f4e0128d7e4079d817169b8af4d7581250e72dc
Artem Trunilin <a.trunilin92@mail.ru>
2023-09-18 23:27:46 +0300

    edits allure

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,11 +13,11 @@ print(PROJECT_ROOT_PATH)
 RESOURCE_PATH = os.path.abspath(os.path.join(PROJECT_ROOT_PATH, 'resources'))
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_managment():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    browser.config.window_width = 900
-    browser.config.window_height = 800
+# @pytest.fixture(scope='function', autouse=True)
+# def browser_managment():
+#     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+#     browser.config.window_width = 900
+#     browser.config.window_height = 800
 
 
 
